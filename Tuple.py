cae5522b63b313a4850e5b18b6f2f068de8853aa
Foo-Maker <github@martin-mitnick.de>
2021-08-19 21:26:49 +0200

    Tuples

---
+++ Tuple.py
@@ -0,0 +1,8 @@
+# Tuples werden ähnlich einer Variable definiert. 
+# Der unterschied ist, dass man hier die Werte in Klammern setzt.
+# 
+# Im folgendem Beispiel habe ich mal eine Liste mit 3 Tuples erstellt. 
+
+coordinates = [(4, 5), (6, 7), (8, 9)]
+#coordinates[1] = 22    # Tuples lassen sich nicht ändern
+print(coordinates[0])
