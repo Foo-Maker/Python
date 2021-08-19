70e047fc54d9e8f16ade41232a2e5354d73a9750
Foo-Maker <github@martin-mitnick.de>
2021-08-19 21:59:01 +0200

    Funktionen

---
+++ Funktionen.py
@@ -0,0 +1,27 @@
+# Funktionen werden erstellt in dem man erstmal   def   eintippt. Damit sagt man Python das eine Funktion definiert wird.
+# Danach kommt der Name der Funktion. Wie bei Variablen sollte der Name aussagekräftig sein.
+# Dann (): 
+#
+# Danach kommt der Code dar Funktion. Dieser muss passend eingerückt sein. Die Funktion endet durch das nicht mehr eingerückte.
+
+def say_hi ():
+    print("Hallo User")
+
+# Um die Funktion aufzurufen schreiben wir einfach den Namen gefolgt von ()
+# In diesen Beispiel say_hi()
+
+print("Oben")
+say_hi()
+print("Unten")
+
+# Natürlich kann man einer Funktion auch Parameter übergeben.
+# Machen wir es also etwas komplexer
+
+def greet_user(name, alter):
+    print("Hallo " + name + ", du bist also " + alter + "Jahre alt.") 
+
+greet_user(input("Bitte Namen angeben.: "), input("Wie alt bist denn du? "))
+
+# Wie wir hier sehen habe ich in der Definition angegeben das ich zwei Werte übergeben möchte.
+# Die abfrage der Werte habe ich direkt in den Aufruf gelegt. Sicher hätte man den Umweg über Variable machen können,
+# allerdings geht es so schneller. (Das andere wäre nur für die Lesbarkeit schöner gewesen)
