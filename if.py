abcdc21e79bcf26f887c61790ff27a3c74cc39a9
Foo-Maker <github@martin-mitnick.de>
2021-08-20 21:56:32 +0200

    if und else

--- if.py
+++ if.py
@@ -32,8 +32,33 @@ else:
 if is_male and is_tall:                  #Wenn beide Werte wahr sind, dann  (eins alleine reicht nicht)
     print("Du bist Mänlich und groß")
 elif not(is_male) and is_tall:           #Durch not() wird der wert in das Gegenteil verkehrt (True wird False und andersrum)
-    print("Da bist eine große Frau")
+    print("Du bist eine große Frau")
 elif (is_male) and not(is_tall):
-    print("Da bist ein kleiner Mann")
+    print("Du bist ein kleiner Mann")
 else:
     print("Du bist einu kleine Frau")
+
+
+# Man muss ja nicht direck ein boolean rein werfen. Es ist auch möglich mit hilfe von Operatoren die zu erzeugen.
+# Im folgenden Codeschnipsel vergleichen wir Werte und geben den größten wieder aus.
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+nu1 = input("Bitte die erste Zahl eingeben.: ")
+nu2 = input("Bitte die zwiete Zahl eingeben.: ")
+nu3 = input("Bitte die dritte Zahl eingeben.: ")
+
+print("Die größte Zahl war " + str(max_num(float(nu1), float(nu2), float(nu3))))
+
+# Auch andere Variablentypen können verglichen werden. Hier ein Beispiel von Strings
+
+if "Otto" == input("Wie heißt Du? "):
+    print("Du bist Otto.")
+else:
+    print("Du bist nicht Otto!")
