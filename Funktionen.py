3804be56ede7057b72e0e68a65f5fb9e80757892
Foo-Maker <github@martin-mitnick.de>
2021-08-20 20:53:49 +0200

    return wurde hinzugefügt

--- Funktionen.py
+++ Funktionen.py
@@ -25,3 +25,12 @@ greet_user(input("Bitte Namen angeben.: "), input("Wie alt bist denn du? "))
 # Wie wir hier sehen habe ich in der Definition angegeben das ich zwei Werte übergeben möchte.
 # Die abfrage der Werte habe ich direkt in den Aufruf gelegt. Sicher hätte man den Umweg über Variable machen können,
 # allerdings geht es so schneller. (Das andere wäre nur für die Lesbarkeit schöner gewesen)
+
+
+
+def cube(num):
+    return num*num*num  #Durch return wird hier der Wert an print zurück gegeben.
+                        #Ohne return würde das Ergebniss zwar ausgerechnet, aber an nichts übergeben.
+    print("Durch den return Befehl wird die Funktion beendet. Dieser Text wird nicht wiedergegeben!")   # Ohne Funktion durch den return Befehl
+    
+print(cube(4))
