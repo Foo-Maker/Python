dc27af8fb97ec3574e42d6fc6cc20f5830fd1445
Foo-Maker <github@martin-mitnick.de>
2021-08-21 07:33:21 +0200

    Ein minimalistischer Taschenrechner

---
+++ Taschenrechner.py
@@ -0,0 +1,12 @@
+num1 = float(input("Bitte die erste Zahl eingeben.: "))
+operator = input("Bitte das Rechenzeichen eingeben.: ")
+num2 = float(input("Bitte die zweite Zahl eingeben.: "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
