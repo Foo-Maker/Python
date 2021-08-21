8b274c78659d8e0e0d99ddafa4ad53af8dd2b895
Foo-Maker <github@martin-mitnick.de>
2021-08-21 08:07:44 +0200

    while Schleife

---
+++ while_Schleife.py
@@ -0,0 +1,6 @@
+i = 1
+while i <= 10: # Eine while Schleife läuft so lange, wie die Bedingung True ist.
+    print(i)
+    i += 1      # i += 1 ist durch den Operator das gleiche wie i = i + 1
+
+print("Fertig")
