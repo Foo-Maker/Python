803f11383396d497b3ed92d45beeae6a0b4d17e9
Foo-Maker <github@martin-mitnick.de>
2021-08-22 15:43:45 +0200

    Kurze Einführung in die Kommentare

---
+++ Kommentare.py
@@ -0,0 +1,17 @@
+# Kommentare sind wichtig und sinnvoll 
+# ein einfacher Weg ist es, mit der Raute (Hashtag) 
+# Dann werden alle folgenden Zeichen in der Zeile ignoriet wenn man das Programm ausführt.
+
+# print("Dieser Text wird nicht erschienen")
+
+'''
+Altenativ kann man auch die drei Apostrophe setzten.
+Damit makiert man den Anfang eines Blocks.
+Dies ist natürlich pracktisch, wenn man viel Text schreiben muss um den Kommentar verständlich abzulegen.
+
+print("Auch diesen Text wird man beim ausführen nie lesen.")
+
+Selbstverständlich müssen die drei Apostrophe auch an das Ende, damit der Interpreter wies wo die Kommentare aufhören.
+'''
+
+print("Dieser Text funktioniert weil er nicht auskommentiert ist.")
